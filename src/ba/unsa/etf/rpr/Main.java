b3d0bc081805f5a0401eddb5e9d4b1b1b035083e
Benjamin <bazman1@etf.unsa.ba>
2021-12-07 15:24:00 +0100

    Dodana nova linija

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Proba za git");
+    System.out.println("Nova linija");
+    }
+}
