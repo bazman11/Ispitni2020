b3c71a791c7afd3d2f2282a678fe0870388b3e80
bazman11 <bazman1@etf.unsa.ba>
2021-12-07 15:31:16 +0100

    Dodana nova Klasa

---
+++ src/ba/unsa/etf/rpr/Klasica.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+public class Klasica {
+    public int broj;
+
+    public int getBroj() {
+        return broj;
+    }
+
+    public void setBroj(int broj) {
+        this.broj = broj;
+    }
+}
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Proba za git");
     System.out.println("Nova linija");
+    System.out.println("Proba za 2");
     }
 }
